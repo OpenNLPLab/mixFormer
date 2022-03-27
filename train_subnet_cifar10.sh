922fc4bab7ace7a114ad2e487954844a365fdcfe
liuzexiang <liuzexiang.sensetime.com>
2022-03-28 01:35:48 +0800

    fix shell

--- train_subnet_cifar10.sh
+++ train_subnet_cifar10.sh
@@ -1,8 +1,8 @@
 T=`date +%m%d%H%M`
-TASK=cifar10_subnet_mixformer_87.4
+TASK=cifar10_subnet_mixformer_latencytest
 g=$(($1<8?$1:8)) ##--distributed-port 12343
 spring.submit run --gpu -n$1 --ntasks-per-node $g  -p MMG \
-"python train.py  \
+"python train.py \
  --sub-configs=configs/cifar10/subtransformer/supnet.yml \
- --configs=configs/cifar10_mixformer/loss_89.07_11.52.yml \
+ --configs=configs/cifar10/search_mixformer/test.yml \
  2>&1 |tee checkpoints/$TASK/log.train.$T "
